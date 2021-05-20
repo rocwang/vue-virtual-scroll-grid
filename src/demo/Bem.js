cdb09442cc7cf8be9f5acfe6867b1de5313c0914
Roc Wong <roc@kiwiberry.nz>
2021-05-20 22:45:12 +1200

    fix: build with lodash

--- src/demo/Bem.js
+++ src/demo/Bem.js
@@ -1,23 +1,25 @@
-import kebabCase from 'lodash/kebabCase'
+import kebabCase from "lodash-es/kebabCase";
 
 export default {
   methods: {
-    bem (element, modifier, outputElement = false) {
-      const blockClass = kebabCase(this.$options.className || this.$options.name)
-      const elementClass = `${blockClass}__${element}`
-      const elementModifierClass = `${elementClass}--${modifier}`
+    bem(element, modifier, outputElement = false) {
+      const blockClass = kebabCase(
+        this.$options.className || this.$options.name
+      );
+      const elementClass = `${blockClass}__${element}`;
+      const elementModifierClass = `${elementClass}--${modifier}`;
 
       if (element && modifier && outputElement) {
-        return `${elementClass} ${elementModifierClass}`
+        return `${elementClass} ${elementModifierClass}`;
       } else if (element && modifier && !outputElement) {
-        return elementModifierClass
+        return elementModifierClass;
       } else if (element && !modifier) {
-        return elementClass
+        return elementClass;
       } else if (modifier) {
-        return `${blockClass}--${modifier}`
+        return `${blockClass}--${modifier}`;
       } else {
-        return blockClass
+        return blockClass;
       }
-    }
-  }
-}
+    },
+  },
+};
